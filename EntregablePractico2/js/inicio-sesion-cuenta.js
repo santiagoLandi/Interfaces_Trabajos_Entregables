de103670d3005d20464d159d25834dbe2451a65a
Ivan <zarateivan648@gmail.com>
2024-10-09 13:35:54 -0300

    Le agregue la ruta a los botones de inicio sesion e iniciar sesion cuenta

--- EntregablePractico2/js/inicio-sesion-cuenta.js
+++ EntregablePractico2/js/inicio-sesion-cuenta.js
@@ -11,19 +11,24 @@ form.addEventListener("submit",e=>{
    
     let esValido = true;
     let patronCorreo = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
-   
+    console.log(esValido);
     if(!patronCorreo.test(correo_sesion.value)){
         document.getElementById("ctrl-email-sesion").textContent = "Ingrese una direccion de correo valida"
         esValido=false;
     }
+    console.log(esValido);
+    console.log(clave_sesion.value);
     let patronClave = /^(?=.*[A-Z])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{8,}$/;
-    if(!patronClave.test(clave_sesion)){
-        document.getElementById("ctrl-password-sesion").textContent = "Debe contener 1mayus y 1 caract especial"
+    if(!patronClave.test(clave_sesion.value)){
+        document.getElementById("ctrl-password-sesion").textContent = "Debe contener 1 mayuscula y 1 caracter especial"
         esValido=false;
     }
+
+    console.log(esValido);
    
     if(esValido){
         form.submit()
+        window.location.href = "index.html";
     }
 
 })
--- EntregablePractico2/js/inicio-sesion.js
+++ EntregablePractico2/js/inicio-sesion.js
@@ -8,15 +8,12 @@ let btn_iniciar_sesion_facebook = document.querySelector("#btn-iniciar-sesion-fa
 
 let btn_iniciar_sesion_registrate = document.querySelector("#btn-iniciar-sesion-registrate");
 
-let btn_iniciar = document.querySelector("#btn-iniciar").addEventListener('click', irAlHome);
-
-
-btn_iniciar_sesion.addEventListener('click', function(){
-    window.location.href = "inicio-sesion-cuenta.html";});
 
 btn_iniciar_sesion_google.addEventListener('click', irAlHome);
 btn_iniciar_sesion_facebook.addEventListener('click', irAlHome);
 
+btn_iniciar_sesion.addEventListener('click', function(){
+    window.location.href = "inicio-sesion-cuenta.html";});
 
 btn_iniciar_sesion_registrate.addEventListener('click',function(){
     window.location.href = "registro.html";
