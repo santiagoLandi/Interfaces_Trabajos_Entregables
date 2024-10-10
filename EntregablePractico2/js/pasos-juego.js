21b8d2f23c072b3c081e505ac67e6b80e7794442
Ivan <zarateivan648@gmail.com>
2024-10-10 18:13:01 -0300

    Se agrego la parte de comentarios

--- EntregablePractico2/js/pasos-juego.js
+++ EntregablePractico2/js/pasos-juego.js
@@ -1,7 +1,19 @@
 "use strict";
 let indice = 0;
 let galeria = ["assets/imagenes/paso1-instrucciones.jpeg", "assets/imagenes/paso2-instrucciones.jpeg", "assets/imagenes/paso3-instrucciones.jpeg", "assets/imagenes/paso4-instrucciones.jpeg"];
+
+let btnVolver = document.querySelector("#btn-volver");
+
+let btnPaso1 = document.querySelector("#paso1");
+let btnPaso2 = document.querySelector("#paso2");
+let btnPaso3 = document.querySelector("#paso3");
+let btnAjustes = document.querySelector("#ajustes");
+
 window.addEventListener("load", mostrarImagen);
+
+btnVolver.addEventListener("click",function(){
+    window.location.href = "index.html";
+});
 
 
 function mostrarImagen() {
@@ -11,15 +23,24 @@ function mostrarImagen() {
 }
 
 
-let btnDer = document.querySelector("#der").addEventListener('click', function () {
-    if (indice < galeria.length - 1) {
-        indice++;
+
+btnPaso1.addEventListener('click', function () {
+        indice = 0;
         mostrarImagen();
-    }
 });
-let btnIzq = document.querySelector("#izq").addEventListener('click', function () {
-    if (indice > 0) {
-        indice--;
-        mostrarImagen();
-    }
+
+btnPaso2.addEventListener('click', function () {
+    indice = 1;
+    mostrarImagen();
 });
+
+btnPaso3.addEventListener('click', function () {
+    indice = 2;
+    mostrarImagen();
+});
+
+btnAjustes.addEventListener('click', function () {
+    indice = 3;
+    mostrarImagen();
+});
+
