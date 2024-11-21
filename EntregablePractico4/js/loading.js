5a288bcdb37c2d0a55213b5c96df6fd840fb1f66
IgnacioMurgolo <ignaciomurgolo@gmail.com>
2024-11-21 11:46:11 -0300

    cambia ruta imagen loading

--- EntregablePractico4/js/loading.js
+++ EntregablePractico4/js/loading.js
@@ -1,27 +1,26 @@
 "use strict";
-
 document.addEventListener("DOMContentLoaded", function () {
-    const loadingProgress = document.getElementById("loading-progress");
-  
-    let percentage = 0;
-    const updatePercentage = () => {
-      loadingProgress.textContent = percentage + "%";
-    };
-  
-    const incrementPercentage = () => {
-      if (percentage < 100) {
-        percentage += 1;
-        updatePercentage();
-        setTimeout(incrementPercentage, 50);
-      } else {
-        setTimeout(function () {
-          document.querySelector(".container-pagina").style.display = "block";
-          document.querySelector("#loader-container").style.display = "none";
-        }, 60);
-      }
-    };
-  
-    // Inicia la simulación
-    incrementPercentage();
-  });
-  
+  const loadingProgress = document.getElementById("loading-progress");
+
+  let percentage = 0;
+  const updatePercentage = () => {
+    loadingProgress.textContent = percentage + "%";
+  };
+
+  const incrementPercentage = () => {
+    if (percentage < 100) {
+      percentage += 1;
+      updatePercentage();
+      setTimeout(incrementPercentage, 50);
+    } else {
+      setTimeout(function () {
+        document.querySelector(".container-pagina").style.display = "block";
+        document.querySelector("#loader-container").style.display = "none";
+      }, 60);
+    }
+  };
+
+  // Inicia la simulación
+  incrementPercentage();
+});
+
