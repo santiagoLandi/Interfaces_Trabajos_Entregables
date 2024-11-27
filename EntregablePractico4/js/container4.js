b46509f37c0dba7e5b2cb72e4b0612d9101fca6b
Santiago Landi <slandi1992@gmail.com>
2024-11-27 16:46:21 -0300

    Se realizan ajustes en el menu hammburguesa y en el contenedor4

--- EntregablePractico4/js/container4.js
+++ EntregablePractico4/js/container4.js
@@ -12,14 +12,17 @@
       let _mouseX = e.clientX;
       let _mouseY = e.clientY;
   
-      // 
+      /*
       let _depth1 = `${50 + (_mouseX - _w) * 0.01}% ${50 + (_mouseY - _h) * 0.01}%`;
       let _depth2 = `${50 + (_mouseX - _w) * 0.02}% ${50 + (_mouseY - _h) * 0.02}%`;
       let _depth3 = `${50 + (_mouseX - _w) * 0.06}% ${50 + (_mouseY - _h) * 0.06}%`;
-  
+      */
+      let _depth = `${50 + (_mouseX - _w) * 0.1}% ${50 + (_mouseY - _h) * 0.1}%`;
+      /*
       let position = `${_depth3}, ${_depth2}, ${_depth1}`;
   
       console.log(position);
-      elem.style.backgroundPosition = position;
+      */
+      elem.style.backgroundPosition = _depth;
     }
   })();
--- EntregablePractico4/js/scroll.js
+++ EntregablePractico4/js/scroll.js
@@ -17,15 +17,19 @@ window.addEventListener("scroll", function() {
     }
 
     const scrollY = window.scrollY;
+
+    
   
     // Controla cada capa individualmente
-
-    document.querySelector(".layer1").style.transform = `translateY(${scrollY * 0.1}px)`;
-    document.querySelector(".layer2").style.transform = `translateY(${scrollY * 0.3}px)`;
-    document.querySelector(".layer3").style.transform = `translateY(${scrollY * 0.4}px)`;
-    document.querySelector(".layer4").style.transform = `translateY(${scrollY * 0.6}px)`;
-    document.querySelector(".layer5").style.transform = `translateY(${scrollY * 0.8}px)`;
-    document.querySelector(".layer6").style.transform = `translateY(${scrollY}px)`;
+    if(scrollY>=0 && scrollY<=270){
+        document.querySelector(".layer1").style.transform = `translateY(${scrollY * 0.1}px)`;
+        document.querySelector(".layer2").style.transform = `translateY(${scrollY * 0.3}px)`;
+        document.querySelector(".layer3").style.transform = `translateY(${scrollY * 0.5}px)`;
+        document.querySelector(".layer4").style.transform = `translateY(${scrollY * 0.55}px)`;
+        document.querySelector(".layer5").style.transform = `translateY(${scrollY * 0.7}px)`;
+        document.querySelector(".layer6").style.transform = `translateY(${scrollY}px)`;
+    }
+    
 
 });
 
